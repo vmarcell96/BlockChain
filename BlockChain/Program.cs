7beb32dad5d3f068253792de0a44a2801611610b
vmarcell96 <vajdam96@gmail.com>
2022-10-05 15:33:24 +0200

    starter files

---
+++ BlockChain/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace BlockChain
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var secretKey = SecretKeyGenerator.GenerateSecretKey();
+            //Console.WriteLine(secretKey);
+
+            var testCoin = new BlockChain();
+            Block testBlock1 = new Block( " Amount : 20 ");
+            Block testBlock2 = new Block(" Amount : 30 ");
+            testCoin.AddNewBlock(testBlock1);
+            testCoin.AddNewBlock(testBlock2);
+
+            //testBlock2.Data = "Amount 50";
+
+            Console.WriteLine(testCoin.IsValidChain());
+        }
+    }
+}
+
+
