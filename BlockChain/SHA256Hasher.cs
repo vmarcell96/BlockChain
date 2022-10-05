a486eda1e914cc28f2abed7d14ef4fdd21acdc5d
vmarcell96 <vajdam96@gmail.com>
2022-10-05 15:32:52 +0200

    created hasher utility class

---
+++ BlockChain/SHA256Hasher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    public static class SHA256Hasher
+    {
+        public static string ComputeStringToSha256Hash(string plainText)
+        {
+            // Create a SHA256 hash from string   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Computing Hash - returns here byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+
+                // now convert byte array to a string   
+                StringBuilder stringbuilder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    // string is formatted in hexadecimal if argument is "x2"
+                    stringbuilder.Append(bytes[i].ToString());
+                }
+                return stringbuilder.ToString();
+            }
+        }
+    }
+}
