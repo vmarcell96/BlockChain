d1abc2c88087f9348774b91119f476e3dbeecf33
vmarcell96 <vajdam96@gmail.com>
2022-10-12 12:47:57 +0200

    test case

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -1,23 +1,29 @@
 ﻿
-
 namespace BlockChain
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            //var secretKey = SecretKeyGenerator.GenerateSecretKey();
-            //Console.WriteLine(secretKey);
-
-            var testCoin = new BlockChain();
-            Block testBlock1 = new Block( " Amount : 20 ");
-            Block testBlock2 = new Block(" Amount : 30 ");
-            testCoin.AddNewBlock(testBlock1);
-            testCoin.AddNewBlock(testBlock2);
-
-            //testBlock2.Data = "Amount 50";
-
-            Console.WriteLine(testCoin.IsValidChain());
+            try
+            {
+                var testCoin = new BlockChain(1);
+                Block testBlock1 = new Block(" Amount : 20 ");
+                Block testBlock2 = new Block(" Amount : 30 ");
+                testCoin.AddNewBlock(testBlock1);
+                testCoin.AddNewBlock(testBlock2);
+                Console.Clear();
+                Console.WriteLine("All blocks are added");
+                if (!testCoin.IsValidChain())
+                {
+                    throw new Exception("The blockchain is invalid.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
